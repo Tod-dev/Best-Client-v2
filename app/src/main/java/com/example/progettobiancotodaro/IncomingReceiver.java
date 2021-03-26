5a0f196965d08c0c29d9138b8cd0129b11f1de75
Michy2690 <270801@studenti.unimore.it>
2021-03-26 17:22:09 +0100

    cancello file

---
+++ app/src/main/java/com/example/progettobiancotodaro/IncomingReceiver.java
@@ -0,0 +1,46 @@
+package com.example.progettobiancotodaro;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+public class IncomingReceiver extends BroadcastReceiver {
+
+    Context context;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        this.context = context;
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        CallListener cl = new CallListener();
+        tm.listen(cl, PhoneStateListener.LISTEN_CALL_STATE);
+    }
+
+    private class CallListener extends PhoneStateListener{
+        public void onCallStateChanged(int state, String incomingNumber){
+            String stato;
+
+            switch(state){
+                case TelephonyManager.CALL_STATE_IDLE: {
+                    stato = "The phone is idle";
+                    break;
+                }
+                case TelephonyManager.CALL_STATE_OFFHOOK: {
+                    stato = "The phone is in use";
+                    break;
+                }
+                case TelephonyManager.CALL_STATE_RINGING:{
+                    stato = "The phone is ringing";
+                    break;
+                }
+                default: stato="null";
+                    break;
+            }
+            Toast.makeText(context, stato+" "+incomingNumber, Toast.LENGTH_SHORT).show();
+
+            //MainActivity.tv.setText(stato+" "+incomingNumber);
+        }
+    }
+}
