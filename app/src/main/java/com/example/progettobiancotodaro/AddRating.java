6ecef48ae5a213726c12368267925dee93cd6bc7
Michy2690 <270801@studenti.unimore.it>
2021-03-28 15:39:52 +0200

    modifiche grafiche

--- app/src/main/java/com/example/progettobiancotodaro/AddRating.java
+++ app/src/main/java/com/example/progettobiancotodaro/AddRating.java
@@ -7,7 +7,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.CallLog;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
