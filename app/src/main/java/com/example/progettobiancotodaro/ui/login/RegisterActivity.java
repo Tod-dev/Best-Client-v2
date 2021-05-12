fe73a74279aedcd618194b527605aca02abae2ad
Michy2690 <270801@studenti.unimore.it>
2021-05-12 17:09:11 +0200

    aggiunta controllo email già esistente nella registrazione

--- app/src/main/java/com/example/progettobiancotodaro/ui/login/RegisterActivity.java
+++ app/src/main/java/com/example/progettobiancotodaro/ui/login/RegisterActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +22,8 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
@@ -96,6 +99,16 @@ public class RegisterActivity extends AppCompatActivity {
 
                         Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
                         startActivity(intent);
+                    }
+                    else{
+                        try {
+                            throw task.getException();
+                        }catch(FirebaseAuthUserCollisionException e) {
+                            email.setError("Email already in use");
+                            email.requestFocus();
+                        }catch(Exception e) {
+                            Log.e("TAG", e.getMessage());
+                        }
                     }
                 });
 
