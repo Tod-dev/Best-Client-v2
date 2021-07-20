2beab9201d9dd7c79c989331de773947a10ce97f
Marco Todaro <marcoposta59@gmail.com>
2021-07-20 10:02:36 +0200

    group by number

--- app/src/main/java/it/bestclient/android/HomeActivity.java
+++ app/src/main/java/it/bestclient/android/HomeActivity.java
@@ -33,6 +33,7 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
